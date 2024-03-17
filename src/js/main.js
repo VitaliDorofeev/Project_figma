428c8004374b5dfe4b5f3093512b9dfc29051be3
Dobbriik <Sashab441@mail.ru>
2024-03-17 14:26:07 +0300

    add rood

--- src/js/main.js
+++
@@ -1,5 +0,0 @@
-document.body.textContent = 'Так лучше '
-document.body.textContent = 'Так лучше 2'
-document.body.textContent = 'Так лучше 3'
-document.body.textContent = 'Так лучше 4'
-document.body.textContent = 'Так лучше 5'
